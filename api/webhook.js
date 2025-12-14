8ec6abab0bced8baa47978457288516acf20d8a6
massssskar <masterskarf@gmail.com>
2025-12-14 23:03:34 +0300

    Update webhook.js

--- api/webhook.js
+++ api/webhook.js
@@ -1,7 +1,14 @@
 export default async function handler(req, res) {
-  if (req.method !== "POST") {
-    return res.status(405).json({ error: "Only POST allowed" });
-  }
+  if (req.method !== "POST") return res.status(405).json({ error: "Only POST allowed" });
+
+  const webhookURL = process.env.DISCORD_WEBHOOK_URL;
+  if (!webhookURL) return res.status(500).json({ error: "Webhook not set" });
+
+  await fetch(webhookURL, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(req.body)
+  });
 
   res.status(200).json({ success: true });
 }
